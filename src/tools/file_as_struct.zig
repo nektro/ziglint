daefcdb2a92f2d13342228b79679e7d0e9e8e87b
Meghan Denny <hello@nektro.net>
2022-01-07 17:57:36 -0800

    rule/file_as_struct- handle inverse case too

--- src/tools/file_as_struct.zig
+++ src/tools/file_as_struct.zig
@@ -17,8 +17,12 @@ pub fn work(alloc: std.mem.Allocator, file_name: []const u8, src: *main.Source, 
     } else false;
 
     const has_lower_name = std.ascii.isLower(std.fs.path.basename(file_name)[0]);
+    const has_upper_name = std.ascii.isUpper(std.fs.path.basename(file_name)[0]);
 
     if (has_top_level_fields and has_lower_name) {
         try writer.print("./{s}:{d}:{d}: found top level fields, file name should be capitalized\n", .{ file_name, 1, 1 });
+    }
+    if (!has_top_level_fields and has_upper_name) {
+        try writer.print("./{s}:{d}:{d}: found no top level fields, file name should be lowercase\n", .{ file_name, 1, 1 });
     }
 }
